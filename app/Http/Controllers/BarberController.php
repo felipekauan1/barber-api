818705195109f34a0b08c865c9eda36a6753b669
Felipe Kauãn <felipekauan30@outlook.com>
2026-04-02 09:52:06 -0300

    chore: criação para dados iniciais de barbeiros

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BarberController extends Controller
 {
-    //
+    private $loggedUser;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->loggedUser = auth()->user();
+    }
+
+    public function createRandom()
+    {
+        $array = ['error'=>''];
+
+        return $array;
+    }
+
+    public function list()
+    {
+        
+    }
 }
